5f84fd0a34bf26be77f6fd7d04324b3e426f9a49
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-01 12:08:46 -0300

    feat: initial config and structure

---
+++ src/screens/Groups/Groups.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from "react-native";
+
+const Groups = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Groups Component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
+export default Groups;

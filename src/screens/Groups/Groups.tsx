bbe521d61166be4bf8eac6e3768f901811c3ab5d
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-05 11:54:12 -0300

    feat: using theme

--- src/screens/Groups/Groups.tsx
+++ src/screens/Groups/Groups.tsx
@@ -1,9 +1,8 @@
-import {Container, Title} from './styles'
+import {Container} from './styles';
 
 const Groups = () => {
   return (
     <Container>
-      <Title>Groups Component</Title>
     </Container>
   );
 };

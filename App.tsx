bed4d4cee753a0518e73d68fd84353a7a8aa30ed
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-05 16:18:04 -0300

    feat: added custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import Groups from "@screens/Groups/Groups";
 import {ThemeProvider} from 'styled-components';
 import defaultTheme from './src/theme/default';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import {ActivityIndicator} from "react-native";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Groups/>
+      {fontsLoaded ? <Groups/> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
